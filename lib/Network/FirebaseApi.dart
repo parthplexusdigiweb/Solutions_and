b278c6016112a1ffe58cfe97c7cc91d57b81b5d4
Fenil Patel <155422780+fenilpatel12@users.noreply.github.com>
2024-04-24 13:28:08 +0530

    er
    frf

--- lib/Network/FirebaseApi.dart
+++ lib/Network/FirebaseApi.dart
@@ -533,8 +533,7 @@ class ApiRepository{
         headers : {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
-          'api-key': "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"
-
+          // 'api-key': "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"
         },
         body: jsonEncode({
           "sender": {
